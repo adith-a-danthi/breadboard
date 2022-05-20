5055d4a9f926a20377584b5a796a6700b54f2ea8
Adith A Danthi <adith.danthi@gmail.com>
2022-05-20 23:35:50 +0530

    feat: add auth context

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { makeServer } from './server';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthProvider } from './contexts/auth-context';
 
 import './styles/index.css';
 
@@ -12,7 +13,9 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
