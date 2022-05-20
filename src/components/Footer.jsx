6a753d3507e00806bfbb42fdd12a2175cb2a50ef
Adith A Danthi <adith.danthi@gmail.com>
2022-05-20 22:33:17 +0530

    feat: add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+export default function Footer() {
+  return (
+    <>
+      <hr />
+
+      <footer className="flex justify-center align-center">
+        <div>
+          <div className="flex gap-2">
+            <a href="http://github.com/adith-a-danthi" target="_blank" rel="noopener noreferrer">
+              <span className="fa-brands fa-github fa-xl"></span>
+            </a>
+            <a href="http://linkedin.in/adith-a-danthi" target="_blank" rel="noopener noreferrer">
+              <span className="fa-brands fa-linkedin fa-xl"></span>
+            </a>
+            <a href="http://adith.dev" target="_blank" rel="noopener noreferrer">
+              <span className="fas fa-globe fa-xl"></span>
+            </a>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Navbar } from './Navbar/Navbar';
 export { default as VideoCard } from './VideoCard/VideoCard';
 export { default as CategoryCard } from './CategoryCard/CategoryCard';
+export { default as Footer } from './Footer';
