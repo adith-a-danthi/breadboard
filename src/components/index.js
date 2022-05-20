171ae0b7da9698805084736e4ef2677772869baf
Adith A Danthi <adith.danthi@gmail.com>
2022-05-20 22:32:03 +0530

    feat: add video card component

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,3 @@
 export { default as Navbar } from './Navbar/Navbar';
+export { default as VideoCard } from './VideoCard/VideoCard';
+export { default as CategoryCard } from './CategoryCard/CategoryCard';
