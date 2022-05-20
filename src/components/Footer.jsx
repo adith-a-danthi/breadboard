ae6fbe4b67d6bd3a85472b075ca6704b9078170a
Adith A Danthi <adith.danthi@gmail.com>
2022-05-20 22:46:02 +0530

    fix:  footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,11 @@ export default function Footer() {
             <a href="http://github.com/adith-a-danthi" target="_blank" rel="noopener noreferrer">
               <span className="fa-brands fa-github fa-xl"></span>
             </a>
-            <a href="http://linkedin.in/adith-a-danthi" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://linkedin.com/in/adith-a-danthi/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <span className="fa-brands fa-linkedin fa-xl"></span>
             </a>
             <a href="http://adith.dev" target="_blank" rel="noopener noreferrer">
