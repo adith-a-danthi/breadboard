07d3a1af38c50f6ad35092d3c27e94380883f19f
Adith A Danthi <adith.danthi@gmail.com>
2022-05-20 22:35:54 +0530

    feat: add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
-import { Home } from './views';
+import { Home, Page404 } from './views';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<Page404 />} />
       </Routes>
     </div>
   );
