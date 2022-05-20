fc9f8666c62d77068852fd8efdd00539ab904838
Adith A Danthi <adith.danthi@gmail.com>
2022-05-20 23:38:15 +0530

    feat: add navigation guards

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
-import { Home, Page404 } from './views';
+import { Home, Page404, Login, Register } from './views';
+import { HideForAuth } from './router';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route element={<HideForAuth />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
         <Route path="*" element={<Page404 />} />
       </Routes>
     </div>
