ddb9b40de40fa48abfa301bd8c13c1498aca9eb0
Adith A Danthi <adith.danthi@gmail.com>
2022-05-21 16:45:21 +0530

    feat: add playlist modal

---
+++ src/components/PlaylistModal/PlaylistModal.jsx
@@ -0,0 +1,81 @@
+import './PlaylistModal.css';
+
+import { useState } from 'react';
+import { useUser } from '../../contexts/user-context';
+import {
+  addVideoToPlaylist,
+  createPlaylist,
+  removeVideoFromPlaylist,
+} from '../../utils/playlist-utils';
+
+export default function PlaylistModal({ showModal, setShowModal, createOnly, video }) {
+  const [newPlaylistTitle, setNewPlaylistTitle] = useState('');
+  const {
+    userData: { playlists },
+    dispatchUserData,
+  } = useUser();
+
+  const createPlaylistHandler = () => {
+    if (newPlaylistTitle.length !== 0) {
+      createPlaylist(dispatchUserData, { title: newPlaylistTitle, description: '' });
+      setNewPlaylistTitle('');
+      setShowModal(false);
+    }
+  };
+
+  const videoInPlaylist = (playlist) =>
+    playlist.videos.some((playlistVideo) => playlistVideo._id === video._id);
+
+  const playlistCheckboxHandler = (playlist) => {
+    videoInPlaylist(playlist)
+      ? removeVideoFromPlaylist(dispatchUserData, playlist, video)
+      : addVideoToPlaylist(dispatchUserData, playlist, video);
+  };
+
+  return (
+    <div>
+      {showModal && <div className="bg-overlay"></div>}
+      <dialog className="custom-modal" open={showModal}>
+        <div className="flex justify-end">
+          <button className="btn btn-gray outlined small" onClick={() => setShowModal(false)}>
+            <i className="fas fa-times"></i>
+          </button>
+        </div>
+        {!createOnly && (
+          <>
+            <h5 className="heading-5 mb-2">Select a Playlist</h5>
+            <ul className="list-group">
+              {playlists?.map((playlist) => (
+                <li className="text-base my-2" key={playlist._id}>
+                  <input
+                    className="input"
+                    type="checkbox"
+                    id={playlist.title}
+                    name={playlist.title}
+                    checked={videoInPlaylist(playlist)}
+                    onChange={() => playlistCheckboxHandler(playlist)}
+                  />
+                  <label className="text-base ml-2" htmlFor={playlist.title}>
+                    {playlist.title}
+                  </label>
+                </li>
+              ))}
+            </ul>
+            <hr />
+          </>
+        )}
+        <h5 className="heading-5 mb-2">Create a Playlist</h5>
+        <input
+          type="email"
+          className="input mb-4"
+          placeholder="Playlist Title"
+          value={newPlaylistTitle}
+          onChange={(e) => setNewPlaylistTitle(e.target.value)}
+        />
+        <button className="btn btn-gray" onClick={() => createPlaylistHandler()}>
+          Create Playlist
+        </button>
+      </dialog>
+    </div>
+  );
+}
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { default as VideoCard } from './VideoCard/VideoCard';
 export { default as CategoryCard } from './CategoryCard/CategoryCard';
 export { default as Footer } from './Footer';
 export { default as VideoListItem } from './VideoListItem/VideoListItem';
+export { default as PlaylistCard } from './PlaylistCard';
+export { default as PlaylistModal } from './PlaylistModal/PlaylistModal';
