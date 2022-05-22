23925a7b70c040cd8a551cc4d7cc00a473cc19f1
Adith A Danthi <adith.danthi@gmail.com>
2022-05-22 21:25:32 +0530

    chore: replace dummy data on home page

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
 import { Navbar, CategoryCard, VideoCard, Footer } from '../../components';
+import { useVideos } from '../../contexts/videos-context';
 
 import './Home.css';
 
 export default function Home() {
+  const { topVideos, categories } = useVideos();
   return (
     <div>
       <main className="pa-4 px-8">
@@ -22,10 +24,11 @@ export default function Home() {
         <section className="mb-8">
           <h3 className="heading-3">Categories</h3>
           <div className="my-4 category-list">
-            <CategoryCard icon="fas fa-mobile fa-xl" title="Phones" />
-            <CategoryCard icon="fas fa-headphones fa-xl" title="Audio" />
-            <CategoryCard icon="fas fa-gamepad fa-xl" title="Gaming" />
-            <CategoryCard icon="fas fa-laptop fa-xl" title="Laptops" />
+            {categories.map(({ _id, categoryName, categoryIcon }) => (
+              <Link to={`/videos?category=${categoryName}`} key={_id}>
+                <CategoryCard title={categoryName} icon={categoryIcon} size="fa-xl" />
+              </Link>
+            ))}
           </div>
         </section>
 
@@ -34,31 +37,11 @@ export default function Home() {
         <section className="mb-8">
           <h3 className="heading-3">Top Videos</h3>
           <div className="top-videos my-4">
-            <VideoCard
-              title="Pixel 4a"
-              image="https://images.unsplash.com/photo-1598778932246-8d86324faa18?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1636"
-              channel="@Google"
-            />
-            <VideoCard
-              title="Sony WH-1000XM4"
-              image="https://images.unsplash.com/photo-1574494461754-de04dc403eec?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170"
-              channel="@Sony"
-            />
-            <VideoCard
-              title="Super Nintendo"
-              image="https://images.unsplash.com/photo-1602029908656-b54d40a76ad8?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170"
-              channel="@Nintendo"
-            />
-            <VideoCard
-              title="MacBook Air"
-              image="https://images.unsplash.com/photo-1548092372-0d1bd40894a3?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170"
-              channel="@Apple"
-            />
-            <VideoCard
-              title="MacBook Air"
-              image="https://images.unsplash.com/photo-1548092372-0d1bd40894a3?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170"
-              channel="@Apple"
-            />
+            {topVideos?.map(({ _id, title, thumbnail, creator }) => (
+              <Link to={`/videos/${_id}`} key={_id}>
+                <VideoCard title={title} image={thumbnail} channel={creator} />
+              </Link>
+            ))}
           </div>
         </section>
         <Footer />
